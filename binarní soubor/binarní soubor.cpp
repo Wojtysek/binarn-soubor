c9bc4436d9ae516c3312413888612e864ffa74a5
Wojtysek <vojtahr1@post.cz>
2022-01-31 14:13:00 +0100

    Přidejte soubory projektu.

---
+++ binarní soubor/binarní soubor.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main(void) {
+
+	FILE* vystuptxt = fopen("vystuptxt.txt", "w");
+	FILE* vystupbin = fopen("vystupbin.txt", "wb");
+
+	int x = 0x1A2B3C4D;
+	fprintf(vystuptxt, "%d", x);
+	fwrite(&x, sizeof(int), 1, vystupbin);
+
+	fclose(vystuptxt);
+	fclose(vystupbin);
+}
